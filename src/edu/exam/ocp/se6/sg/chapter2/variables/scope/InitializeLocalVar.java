199786a955f433c973658644e4118646e91f2475
EastmanJian <50935@qq.com>
2017-09-22 16:30:16 +0800

    amended variable sample

--- src/edu/exam/ocp/se6/sg/chapter2/variables/scope/InitializeLocalVar.java
+++ src/edu/exam/ocp/se6/sg/chapter2/variables/scope/InitializeLocalVar.java
@@ -6,6 +6,7 @@ package edu.exam.ocp.se6.sg.chapter2.variables.scope;
 public class InitializeLocalVar {
     public int notValid() {
         int y = 10;
+//        double y =10.0; //Compile Error:variable y is already defined in method notValid()
         int x;
 //        int reply = x + y; //Compile Error: variable x might not have been initialized
 //        return reply;
